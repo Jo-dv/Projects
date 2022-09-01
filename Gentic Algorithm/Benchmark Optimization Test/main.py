c0ee4a1f9186df98fef9037d1da25d25402e661e
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2022-09-01 12:34:38 +0900

    Update main.py

--- Gentic Algorithm/Benchmark Optimization Test/main.py
+++ Gentic Algorithm/Benchmark Optimization Test/main.py
@@ -1,19 +1,14 @@
-import numpy as np
-import pandas as pd
 import problems
 from run import algorithms
 
 
-def __main__():
-    problems
-    rosenbrock = np.array(pd.read_csv('problems/rosenbrock_problem.csv', header=None))
-    rastrigin = np.array(pd.read_csv('problems/rastrigin_problem.csv', header=None))
-    griewank = np.array(pd.read_csv('problems/griewank_problem.csv', header=None))
-    shc = np.array(pd.read_csv('problems/shc_problem.csv', header=None))
+def main(algorithm, problem, run, gen, pc, n_c, pm, n_m, w=None):
+    problems.generate()
 
-    test = algorithms(1, 1000, ('rastrigin', rastrigin), 'GA')
-    BSF, BCP, ACP, WCP, MBF, avg_fits = test.evolve(pc=1.0, n_c=10, pm=0.005, n_m=10)
+    test = algorithms(run, gen, (problem, problems.load(problem)), algorithm)
+    BSF, BCP, ACP, WCP, MBF, avg_fits = test.evolve(pc, n_c, pm, n_m, w)
     print(MBF)
 
 
-__main__()
+if __name__ == '__main__':
+    main('GA', 'shc', 10, 150, 1.0, 10, 0.005, 10, 2)
